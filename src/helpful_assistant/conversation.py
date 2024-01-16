2016fe92c06e35d5408e4d9b4ce299b3f127bf3b
Whitelisted <77711834+Whitelisted1@users.noreply.github.com>
2024-01-15 17:00:36 -0700

    Add generation function to conversation object

--- src/helpful_assistant/conversation.py
+++ src/helpful_assistant/conversation.py
@@ -1,4 +1,8 @@
-from typing import List
+from __future__ import annotations
+from typing import List, TYPE_CHECKING, Union, Generator
+
+if TYPE_CHECKING:
+    from .assistant import Assistant
 
 
 class Message:
@@ -8,9 +12,16 @@ class Message:
 
 
 class Conversation:
-    def __init__(self, name: str = "Conversation", history: List[Message] = []):
+    def __init__(self, name: str = "Conversation", history: List[Message] = [], assistant: Union[Assistant, None] = None):
         self.name = name
         self.history = history
+        self.assistant = assistant
 
     def add_to_history(self, message: Message):
         self.history.append(message)
+
+    def generate(self, *args, **kwargs) -> Union[Generator, str]:
+        if self.assistant is None:
+            raise RuntimeError("No assistant object specified in this conversation.")
+
+        return self.assistant.generate(conversation=self, *args, **kwargs)
