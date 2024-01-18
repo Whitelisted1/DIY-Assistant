6c3a8b32dd1e36c007d1bec7db3d2df015a3296f
Whitelisted <77711834+Whitelisted1@users.noreply.github.com>
2024-01-17 18:31:18 -0700

    Complete proof-of-concept generate function

--- src/helpful_assistant/assistant.py
+++ src/helpful_assistant/assistant.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
-from typing import Union, List, Generator, TYPE_CHECKING, Optional
+from typing import Union, List, TYPE_CHECKING, Optional, Any
 from .conversation import Conversation, Message
 from .module import Module
+from .stream import Stream
 
-# if TYPE_CHECKING:
-#     pass
+if TYPE_CHECKING:
+    from .action import Action
 
 
 class Assistant:
@@ -34,27 +35,32 @@ Do not talk about how you are unable to return real-time information. You must O
     def convert_modules_to_llm_readable(self) -> str:
         return "\n".join([f"{module.name} ({module.definition})" for module in self.module_list])
 
-    def generate(self, conversation: Conversation, stream: bool = False, allow_action_execution: bool = True) -> Union[Generator, str]:
-        self._app_action_cycle(conversation)
+    def generate(self, conversation: Conversation, stream: bool = False, allow_action_execution: bool = True) -> Union[Stream, str]:
+        if allow_action_execution:
+            task_output, used_module, used_action = self._app_action_cycle(conversation)
 
-    def _app_action_cycle(self, conversation: Conversation):
+            if task_output is not None:
+                conversation.history[-1].content += f"\n\n(An action was run. Action Output: ```{task_output}```. Use this output in your response, if applicable.)"
+
+        # Make a Stream object that adds the generation result to history once it has completed
+        output = Stream(self.llm.generate(conversation.history, stream=stream), lambda x: conversation._add_to_history(Message("assistant", "".join(x))))
+
+        return output
+
+    def _app_action_cycle(self, conversation: Conversation) -> List[Union[str, None], Union[Module, None], Union[Action, None]]:
         conversation_history = conversation.history.copy()
 
         user_query = conversation_history[-1].content.replace("`", "'") # not a great fix ¯\_(ツ)_/¯
-        modules_prompt = f'''```{user_query}```\n\nGiven the above query, respond with ONLY the name of the module that you would like to find more information about. Any other text or tokens will break the application. If none of the modules are helpful, respond with EXACTLY "null". Do not make up modules.'''
-        conversation_history[-1] = Message("user", modules_prompt)
 
-        print(conversation_history)
+        modules_prompt = f'''```{user_query}```\n\nGiven the above query, respond with ONLY the name of the module that you would like to find more information about. Any other text or tokens will break the application. If none of the modules are helpful, respond with EXACTLY "null". If a module is not needed, respond with EXACTLY "null". Do not make up modules.'''
+        conversation_history[-1] = Message("user", modules_prompt)
 
         output = self.llm.generate(conversation_history, stream=False)
         conversation_history.append(Message("assistant", output))
         output = output.strip().lower()
 
-        print(output)
-
         if output == "null":
-            print("No module")
-            return # No module
+            return None, None, None
 
         active_module = None
         for m in self.module_list:
@@ -63,8 +69,7 @@ Do not talk about how you are unable to return real-time information. You must O
                 break
 
         if active_module is None:
-            print("Module not found")
-            return # Module not found
+            return None, None, None
 
         conversation_history.append(Message("user", f'''The "Weather" module has the following actions:\n```\n{m.convert_actions_to_llm_readable()}\n```\n\nRespond with ONLY the name of the action that you would like to execute. Any other text or tokens will break the application. If you do not wish to execute any of the given actions, respond with EXACTLY "null".'''))
 
@@ -72,11 +77,8 @@ Do not talk about how you are unable to return real-time information. You must O
         conversation_history.append(Message("assistant", output))
         output = output.strip().lower()
 
-        print(output)
-
         if output == "null":
-            print("No action")
-            return # No module
+            return None, active_module, None
 
         active_action = None
         for a in active_module.actions:
@@ -85,9 +87,8 @@ Do not talk about how you are unable to return real-time information. You must O
                 break
 
         if active_action is None:
-            print("Action not found")
-            return
+            return None, active_module, None
 
         task_out = active_action.task()
 
-        print(task_out)
+        return str(task_out), active_module, active_action
